0ef8839fafbc3c635ec1b899c10e6c5a9a616f37
anisul islam <anisulislam980340@gmail.com>
2023-05-14 09:51:26 +0600

    data types completed

---
+++ datatypes.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int a=15;
+    int b=5;
+    float pi=3.14;
+    cout<<"the a value is"<< a<<"the b value is "<< b;
+    cout<<"\nthe value of pi is " <<pi;
+    return 0;
+    }
